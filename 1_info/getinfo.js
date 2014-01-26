797fc963c35c18de36c1a7e3e14e67bc7e29bc57
you21979 <you2197901@gmail.com>
2014-01-26 21:56:13 +0900

    mod path

--- 1_info/getinfo.js
+++ 1_info/getinfo.js
@@ -1,6 +1,6 @@
 var bitcoin = require('bitcoin');
 var fs = require('fs');
-var CONFIG_FILE = "./config/wallet.json";
+var CONFIG_FILE = "../config/wallet.json";
 var client = new bitcoin.Client(JSON.parse(fs.readFileSync(CONFIG_FILE)));
 client.getInfo(function(err, info) {
   console.log(info);
